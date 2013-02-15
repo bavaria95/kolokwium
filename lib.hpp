bea7d831751e2b08843f365304f41652cce52bf3
bavaria95 <bavaria95@gmail.com>
2013-02-14 17:17:57 -0800

    Create lib.hpp
    header's file with functions declarations

---
+++ lib.hpp
@@ -0,0 +1,37 @@
+#include "lib.cpp"
+
+void write_list(list <int>);
+
+void swap(int, int);
+
+void bubbleSort();
+
+void bubbleSort_lists();
+
+void convert_list_to_array_1();
+
+void convert_list_to_array_2();
+
+void convert_array_to_list_1();
+
+void convert_array_to_list_2();
+
+void matrix_to_list_1();
+
+void matrix_to_list_2();
+
+void matrixes_to_list();
+
+void select();
+
+void quickSort_basic(int, int, int);
+
+void from_city(list <int>, short, bool);
+
+void sort_lists();
+
+void sort_bubble();
+
+void sort_quick();
+
+void solution();
